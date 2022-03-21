f865a32f62f2c95e640e63df7b7df3c94b6d1496
Jaume Aloy <hircine45@gmail.com>
2022-03-21 23:44:06 +0100

    trying to understand shared references between threads without success

--- 2-threads/src/main.rs
+++ 2-threads/src/main.rs
@@ -1,7 +1,10 @@
 use std::thread;
 use std::sync::*;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::boxed::Box;
 
-const MAX: i64 = 100000000;
+const MAX: i64 = 10000;
 const THREAD: i8 = 10;
 
 
@@ -14,9 +17,19 @@ fn main() {
 
     let new_counter: Arc<i64> = Arc::new(0);
 
+    let my_counter: Arc<Mutex<Box<i64>>> = Arc::new(Mutex::new(Box::new(0)));
+
+    //let value = my_counter.lock().unwrap();
+    //println!("Simple is {0}", value);
+
+    let mut tmp = Box::new(0);
+    let a = tmp.as_mut();
+    *a = 1;
+    println!("tmp is {0}", tmp);
+   
     for _i in 0..THREAD {
-        let counter_ref = new_counter.clone();
-        let thread = thread::spawn(|| worker(counter_ref));
+        let my_ref = my_counter.clone();
+        let thread = thread::spawn(move || worker(my_ref));
         threads.push(thread);
     }
 
@@ -28,10 +41,16 @@ fn main() {
 }
 
 
-fn worker(counter: Arc<i64>) {
+fn worker(counter: Arc<Mutex<Box<i64>>>) {
     println!("Hello from another thread");
 
     for i in 0..(MAX / (THREAD as i64)) {
+        let my_box = counter.lock().unwrap();
         // TODO: increment the counter
+
     }
+
+    let value = counter.lock().unwrap();
+    println!("Value is {0}", value)
+
 }
