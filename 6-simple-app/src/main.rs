e2c95cbb005460fda6f45c139f837e0806026d3d
Jaume Aloy <hircine45@gmail.com>
2022-04-08 21:22:06 +0200

    edit, delete and view

--- 6-simple-app/src/main.rs
+++ 6-simple-app/src/main.rs
@@ -1,6 +1,8 @@
 mod models;
 mod database;
 mod user_dao;
+use std::io::Write;
+
 use chrono::DateTime;
 use models::{UserCollection, User};
 use mysql_async::prelude::Queryable;
@@ -35,7 +37,54 @@ async fn main() {
             user_dao::create_user(&mut new_user).await;
 
             println!("User inserted with id {}", new_user.id);
+        } else if selected_option == 2 {
+            println!("Insert user id: ");
+            let user_id = integer_input();
+            if user_dao::delete_user(user_id).await {
+                println!("User has been deleted");
+            } else {
+                println!("Couldn't delete user.");
+            }
+        } else if selected_option == 3 {
+            println!("Insert user id: ");
+            let user_id = integer_input();
+
+            if let Some(mut user) = user_dao::get_user(user_id).await {
+                println!("{}", user);
+
+                println!("Insert the new values or leave them blank.");
+                let name = text_input("Name: ");
+                if name.len() > 0 {
+                    user.name = name;
+                }
+
+                let email = text_input("Email: ");
+                if email.len() > 0 {
+                    user.email = email;
+                }
+
+                let age = text_input("Age: ");
+                if age.len() > 0 {
+                    user.age = age.parse().unwrap();
+                }
+
+                user_dao::save_user(&user)
+                    .await;
+
+                println!("Data has been saved.");
+            } else {
+                println!("User does not exist.");
+            }
         } else if selected_option == 4 {
+            println!("Insert user id: ");
+            let user_id = integer_input();
+            
+            if let Some(user) = user_dao::get_user(user_id).await {
+                println!("{}", user);
+            } else {
+                println!("User does not exist.");
+            }
+        } else if selected_option == 5 {
             println!("Good bye!");
             break;
         }
@@ -50,6 +99,7 @@ fn show_menu() -> u8 {
         "Create a new user",
         "Delete an user",
         "Edit an user",
+        "Show an user",
         "Exit"
     );
 
@@ -94,9 +144,49 @@ fn text_input(prompt: &str) -> String {
     let mut buffer = String::new();
 
     print!("{}", prompt);
-    
+    std::io::stdout().flush();
+
     let input = std::io::stdin();
     input.read_line(&mut buffer);
 
-    buffer
+    let mut line_end = 0;
+    for character in buffer.chars() {
+        if character == '\n' {
+            break;
+        }
+
+        line_end += 1;
+    }
+
+    buffer[0..line_end - 1].to_owned()
+}
+
+fn integer_input() -> usize {
+    let mut buffer = String::new();
+    
+
+
+    let mut value: Option<usize> = None;
+    while let None = value {
+        let input = std::io::stdin();
+        input.read_line(&mut buffer);
+    
+        let mut line_end = 0;
+        for character in buffer.chars() {
+            if character == '\n' {
+                break;
+            }
+            line_end += 1;
+
+        }
+
+        let result = buffer[0..line_end - 1].parse::<usize>();
+        if let Ok(x) = result {
+            value = Some(x);
+        } else {
+            println!("Invalid number. Insert a valid number.");
+        }
+    }
+
+    value.unwrap()
 }
--- 6-simple-app/src/user_dao.rs
+++ 6-simple-app/src/user_dao.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::vec;
 
 use chrono::{Utc, DateTime, NaiveDateTime};
@@ -52,4 +53,57 @@ pub async fn create_user<'a>(user: &'a mut User) -> &'a mut User {
 
     user.id = last_id as usize;
     user
-} 
+}
+
+pub async fn get_user(id: usize) -> Option<User> {
+    let mut connection = database::get_connection().await.unwrap();
+
+    let params = Params::Positional(vec![id.to_value()]);
+    let row = connection
+        .exec_first::<Row, &str, Params>("SELECT name, email, age, created FROM users WHERE id = ?", params)
+        .await
+        .unwrap();
+
+    if let Some(result) = row {
+        Some(
+            User {
+                id: id,
+                email: result.get("email").unwrap(),
+                age: result.get("age").unwrap(),
+                name: result.get("name").unwrap(),
+                created: Utc::now()
+            }
+        )
+    } else {
+        None
+    }
+}
+
+pub async fn save_user(user: &User) {
+    let mut connection = database::get_connection().await.unwrap();
+    
+    let params = Params::Named(
+        HashMap::from([
+            ("email".to_owned(), user.email.to_value()),
+            ("name".to_owned(), user.name.to_value()),
+            ("age".to_owned(), user.age.to_value()),
+            ("id".to_owned(), user.id.to_value())
+        ])
+    );
+
+    connection
+        .exec_drop("UPDATE users SET email = :email, name = :name, age = :age WHERE id = :id", params)
+        .await;
+}
+
+pub async fn delete_user(user_id: usize) -> bool {
+    let mut connection = database::get_connection().await.unwrap();
+
+    let params = Params::Positional(vec![user_id.to_value()]);
+
+    connection
+        .exec_drop("DELETE FROM users WHERE id = ? LIMIT 1", params)
+        .await;
+
+    connection.affected_rows() > 0
+}
